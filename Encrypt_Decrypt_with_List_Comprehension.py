2957d01fb6a920c77876d6008c542240dd1a4e1e
Lê Văn Tú <107534756+levantu2003@users.noreply.github.com>
2022-08-26 14:08:34 +0700

    Upload file

---
+++ Encrypt_Decrypt_with_List_Comprehension.py
@@ -0,0 +1,21 @@
+from cgitb import text
+
+
+def encryt_char(char, key):
+    return chr(ord('A') + (ord(char) - ord('A') + key) % 26)
+def decrypt_char(char, key):
+    return encryt_char(char, 26 - key)
+if __name__ == "__main__":
+    choice = int(input("1. Encrypt\n2. Decrypt\nEnter your choice: "))
+    print(f"You choose {choice}!")
+    key = 3
+    if choice == 1:
+        text = input("Enter your text: ")
+        cipher_list = [encryt_char(c,key) if c not in ' ,.' else c for c in text.upper()]
+        print(''.join(cipher_list))
+    elif choice == 2:
+        text = input("Enter your text: ")
+        message_list = [decrypt_char(c, key) if c not in ' ,.' else c for c in text.upper()]
+        print(''.join(message_list))
+    else:
+        print("Invalid choice!")
